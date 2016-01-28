08eac7b090052f6020d6edbb199e72074a140ab5
Leonardo Flores <leo@MacBook-Pro-de-Leonardo.local>
2016-01-28 15:33:26 -0200

    * Adicionando a biblioteca ngMap para gerenciar o Google Maps (bower install --save ngmap) * Alterado o evento ng-show para ng-if da aba Map
    Link da Documentação do ngMap: https://rawgithub.com/allenhwkim/angularjs-google-maps/master/build/docs/index.html
    Link de exemplos com ngMap: https://ngmap.github.io/

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('pitacco', ['ionic','ngCordova','app.controllers','ui.router','ionic.rating','ngOpenFB']);
+var app = angular.module('pitacco', ['ionic','ngCordova','app.controllers', 'ngMap', 'ui.router','ionic.rating','ngOpenFB']);
 
 app.run(function($ionicPlatform, ngFB) {
   //facebook-login
